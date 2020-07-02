7ef5597e23814406196cbc1267ab815646523628
naresh1406 <nareshgupta1406@gmail.com>
2020-07-02 21:38:42 +0700

    lc july 02

---
+++ src/main/cp/leetcode/july/Day_02_Binary_Tree_Level_Order_Traversal_II.java
@@ -0,0 +1,112 @@
+package main.cp.leetcode.july;
+
+import java.util.*;
+
+/**
+ * Day 01 Arranging Coins
+ * Problem Link - https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3378/
+ */
+public class Day_02_Binary_Tree_Level_Order_Traversal_II {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 1. Using Collections Reverse
+    class Solution {
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            if (root == null) return new ArrayList();
+            List<List<Integer>> result = new ArrayList();
+            Queue<TreeNode> queue = new LinkedList();
+
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> level = new ArrayList();
+                while (size-- > 0) {
+                    root = queue.poll();
+                    level.add(root.val);
+                    if (root.left != null)
+                        queue.add(root.left);
+                    if (root.right != null)
+                        queue.add(root.right);
+                }
+                result.add(level);
+            }
+
+            Collections.reverse(result);
+            return result;
+        }
+    }
+
+    // 2.Append
+    class Solution {
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            if (root == null) return new ArrayList();
+            List<List<Integer>> result = new ArrayList();
+            Queue<TreeNode> queue = new LinkedList();
+
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> level = new ArrayList();
+                while (size-- > 0) {
+                    root = queue.poll();
+                    level.add(root.val);
+                    if (root.left != null)
+                        queue.add(root.left);
+                    if (root.right != null)
+                        queue.add(root.right);
+                }
+                result.add(0, level);
+            }
+
+            return result;
+        }
+    }
+
+    // 3. Using Stack
+    class Solution {
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            if (root == null) return new ArrayList();
+            List<List<Integer>> result = new ArrayList();
+            Queue<TreeNode> queue = new LinkedList();
+            java.util.Stack<List<Integer>> stack = new Stack();
+
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> level = new ArrayList();
+                while (size-- > 0) {
+                    root = queue.poll();
+                    level.add(root.val);
+                    if (root.left != null)
+                        queue.add(root.left);
+                    if (root.right != null)
+                        queue.add(root.right);
+                }
+                stack.add(level);
+            }
+
+            while (!stack.isEmpty())
+                result.add(stack.pop());
+
+            return result;
+        }
+    }
+}
