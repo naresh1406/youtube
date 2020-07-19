8244b29bed665781c4d37d06506eeb4a364690b0
naresh1406 <nareshgupta1406@gmail.com>
2020-07-19 18:52:47 +0700

    lc july 19

---
+++ src/main/cp/leetcode/july/Day_19_Add_Binay.java
@@ -0,0 +1,36 @@
+package main.cp.leetcode.july;
+
+import java.math.BigInteger;
+
+/**
+ * Day_19_Add_Binay
+ * Problem Link - https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3395/
+ */
+public class Day_19_Add_Binay {
+    class Solution {
+        public String addBinary(String a, String b) {
+            StringBuilder sb = new StringBuilder();
+            int i = a.length() - 1;
+            int j = b.length() - 1;
+            int carry = 0;
+
+            while (i >= 0 || j >= 0) {
+                int sum = carry;
+                if (i >= 0) sum += a.charAt(i--) - '0';
+                if (j >= 0) sum += b.charAt(j--) - '0';
+                sb.append(sum % 2);
+                carry = sum / 2;
+            }
+
+            if (carry != 0) sb.append(carry);
+
+            return sb.reverse().toString();
+        }
+    }
+
+    class Solution2 {
+        public String addBinary(String a, String b) {
+            return new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2);
+        }
+    }
+}
