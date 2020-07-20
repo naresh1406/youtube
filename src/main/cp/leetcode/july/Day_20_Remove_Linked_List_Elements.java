16c2755c3af170bc66b22b4ef4998a2c0eba6800
naresh1406 <nareshgupta1406@gmail.com>
2020-07-20 23:43:43 +0700

    lc july 20

---
+++ src/main/cp/leetcode/july/Day_20_Remove_Linked_List_Elements.java
@@ -0,0 +1,52 @@
+package main.cp.leetcode.july;
+
+/**
+ * Day_20_Remove_Linked_List_Elements
+ * Problem Link - https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3396/
+ */
+public class Day_20_Remove_Linked_List_Elements {
+    // 1. With dummy head
+    class Solution {
+        public ListNode removeElements(ListNode head, int val) {
+            ListNode dummyNode = new ListNode(-1);
+            dummyNode.next = head;
+            ListNode prev = dummyNode;
+
+            while (head != null) {
+                if (head.val != val) {
+                    prev.next = head;
+                    prev = prev.next;
+                }
+                head = head.next;
+            }
+            prev.next = null;
+            return dummyNode.next;
+        }
+    }
+
+    // 2. Without dummy head
+    class Solution2 {
+        // TC - O(N)
+        // SC - O(1)
+        public ListNode removeElements(ListNode head, int val) {
+            while (head != null && head.val == val) head = head.next;
+            ListNode current = head;
+            while (current != null && current.next != null) {
+                if (current.next.val == val)
+                    current.next = current.next.next;
+                else current = current.next;
+            }
+
+            return head;
+        }
+    }
+
+    // 3. Recursive
+    class Solution3 {
+        public ListNode removeElements(ListNode head, int val) {
+            if (head == null) return null;
+            head.next = removeElements(head.next, val);
+            return head.val == val ? head.next : head;
+        }
+    }
+}
---
+++ src/main/cp/leetcode/july/ListNode.java
@@ -0,0 +1,19 @@
+package main.cp.leetcode.july;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
