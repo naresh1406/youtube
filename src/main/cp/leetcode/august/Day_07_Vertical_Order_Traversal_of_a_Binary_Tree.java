12fff79df3ae3013d43ac5998aa62f2f6d6225cd
naresh1406 <nareshgupta1406@gmail.com>
2020-08-08 01:49:44 +0700

    lc august 07 2

--- src/main/cp/leetcode/august/Day_07_Vertical_Order_Traversal_of_a_Binary_Tree.java
+++ src/main/cp/leetcode/august/Day_07_Vertical_Order_Traversal_of_a_Binary_Tree.java
@@ -52,4 +52,46 @@ public class Day_07_Vertical_Order_Traversal_of_a_Binary_Tree {
             vtUtil(root.right, x + 1, y - 1, list);
         }
     }
+
+    // 2. Solution 2
+    class Solution2 {
+        public List<List<Integer>> verticalTraversal(TreeNode root) {
+            List<List<Integer>> result = new ArrayList();
+            if (root == null) return result;
+
+            TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap();
+            vtUtil(root, 0, 0, map);
+
+            for (Map.Entry<Integer, TreeMap<Integer, PriorityQueue<Integer>>> entry : map.entrySet()) {
+                List<Integer> line = new ArrayList();
+                for (PriorityQueue<Integer> pq : entry.getValue().values()) {
+                    while (pq.size() > 0)
+                        line.add(pq.poll());
+                }
+                result.add(line);
+            }
+
+            return result;
+        }
+
+        private void vtUtil(TreeNode root, int x, int y, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map) {
+            if (root == null)
+                return;
+
+            TreeMap<Integer, PriorityQueue<Integer>> m = new TreeMap<>();
+            PriorityQueue<Integer> pq = new PriorityQueue();
+            if (map.containsKey(x)) {
+                m = map.get(x);
+                if (m.containsKey(y)) {
+                    pq = m.get(y);
+                }
+            }
+            pq.add(root.val);
+            m.put(y, pq);
+            map.put(x, m);
+
+            vtUtil(root.left, x - 1, y + 1, map);
+            vtUtil(root.right, x + 1, y + 1, map);
+        }
+    }
 }
