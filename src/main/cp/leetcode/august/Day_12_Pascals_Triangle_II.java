831faa2800c718ae13cdbf4cfa1373c814317343
naresh1406 <nareshgupta1406@gmail.com>
2020-08-12 21:36:38 +0700

    lc august 12

--- src/main/cp/leetcode/august/Day_12_Pascals_Triangle_II.java
+++ src/main/cp/leetcode/august/Day_12_Pascals_Triangle_II.java
@@ -8,7 +8,7 @@ public class Day_12_Pascals_Triangle_II {
     class Solution {
         public List<Integer> getRow(int rowIndex) {
             Integer[] result = new Integer[rowIndex + 1];
-            Arrays.fill(result, 0)
+            Arrays.fill(result, 0);
             result[0] = 1;
             for (int i = 1; i <= rowIndex; i++)
                 for (int j = i; j > 0; j--)
