007e2d3d2b13c517feb0bfb3233d1898e24673d4
naresh1406 <nareshgupta1406@gmail.com>
2020-08-19 00:58:06 +0700

    lc august 18

---
+++ src/main/cp/leetcode/august/Day_18.java
@@ -0,0 +1,33 @@
+package main.cp.leetcode.august;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day_18 {
+    class Solution {
+        public int[] numsSameConsecDiff(int N, int K) {
+            if (N == 1)
+                return new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            List<Integer> result = new ArrayList<Integer>();
+            for (int num = 1; num < 10; ++num)
+                dfs(N - 1, num, K, result);
+
+            return result.stream().mapToInt(i -> i).toArray();
+        }
+
+        private void dfs(int N, int num, int K, List<Integer> result) {
+            if (N == 0) {
+                result.add(num);
+                return;
+            }
+
+            if (num % 10 + K < 10)
+                dfs(N - 1, num * 10 + num % 10 + K, K, result);
+
+            if (K > 0 && num % 10 - K >= 0)
+                dfs(N - 1, num * 10 + num % 10 - K, K, result);
+        }
+    }
+}
+
