3f4c54da15eb0a94fbc8fe6f6abad1d94a6bb4e8
naresh1406 <nareshgupta1406@gmail.com>
2020-10-09 18:00:03 +0700

    lc ocotber 08 premium

---
+++ src/main/cp/leetcode/october/Day_08_170_Two_Sum_III_Data_Structure_Design.java
@@ -0,0 +1,38 @@
+package main.cp.leetcode.october;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day_08_170_Two_Sum_III_Data_Structure_Design {
+    public class TwoSum {
+
+        private Map<Integer, Integer> map = new HashMap();
+
+        /**
+         * @param number: An integer
+         * @return: nothing
+         */
+        public void add(int number) {
+            // write your code here
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        }
+
+        /**
+         * @param value: An integer
+         * @return: Find if there exists any pair of numbers which sum is equal to the value.
+         */
+        public boolean find(int value) {
+            // write your code hereL
+            for (int key : map.keySet()) {
+                int target = value - key;
+                if (map.containsKey(target)) {
+                    if (key == target && map.get(key) < 2) {
+                        continue;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
