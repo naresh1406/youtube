0c3c99aaa1530ece8daff1c12e6662a4c299f5b1
naresh1406 <nareshgupta1406@gmail.com>
2020-10-12 12:06:49 +0700

    lc _1566_Detect_Pattern_of_Length_M_Repeated_K_or_More_Times 2

--- src/main/cp/leetcode/problems/_1566_Detect_Pattern_of_Length_M_Repeated_K_or_More_Times.java
+++ src/main/cp/leetcode/problems/_1566_Detect_Pattern_of_Length_M_Repeated_K_or_More_Times.java
@@ -5,11 +5,9 @@ public class _1566_Detect_Pattern_of_Length_M_Repeated_K_or_More_Times {
         // TC - O(N)
         // SC - O(1)
         public boolean containsPattern(int[] arr, int m, int k) {
-            int n = arr.length;
-            if (n < m * k) return false;
             int count = 0;
-            for (int i = 0; i + m < n; i++) {
-                count += arr[i] == arr[i + m] ? 1 : 0;
+            for (int i = 0; i + m < arr.length; i++) {
+                count = arr[i] == arr[i + m] ? count + 1 : 0;
                 if (count == (k - 1) * m)
                     return true;
             }
