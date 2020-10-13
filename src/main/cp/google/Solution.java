8fb36bad852a10eab1426dd58c1735335969ab26
naresh1406 <nareshgupta1406@gmail.com>
2020-10-14 01:24:19 +0700

    google Template

---
+++ src/main/cp/google/Solution.java
@@ -0,0 +1,23 @@
+package main.cp.google;
+
+import java.util.Scanner;
+
+/**
+ * This is Common Java Template which will be use for google coding competitions problems.
+ * Detailed Video Explanation - https://www.youtube.com/watch?v=0TB6pE7A9F8
+ */
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 1; i <= t; i++) {
+            // read input
+
+            Object result = "";
+            // solve problem
+
+
+            System.out.println("Case #" + i + ": " + result);
+        }
+    }
+}
