15d71a090d52417161a36248e7c35425094e8152
naresh1406 <nareshgupta1406@gmail.com>
2020-10-19 00:42:14 +0700

    lc _122_Best_Time_to_Buy_and_Sell_Stock_II

---
+++ src/main/cp/leetcode/problems/_122_Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -0,0 +1,13 @@
+package main.cp.leetcode.problems;
+
+public class _122_Best_Time_to_Buy_and_Sell_Stock_II {
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int maxProfit = 0;
+            for (int i = 1; i < prices.length; i++)
+                if (prices[i] > prices[i - 1])
+                    maxProfit += prices[i] - prices[i - 1];
+            return maxProfit;
+        }
+    }
+}
